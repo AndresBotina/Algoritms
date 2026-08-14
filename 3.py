a3ef33a318c7ce215285a03f30fd3fae34471381
Andres Botina <cacbotina@gmail.com>
2026-08-13 21:19:31 -0500

    3

---
+++ 3.py
@@ -0,0 +1,5 @@
+print ("Concatenar numero y texto")
+num1 = input("Ingrese un número: ")
+texto = input("Ingrese un texto: ")
+resultado = str(num1) + " " + texto
+print("El resultado de la concatenación es:", resultado)
