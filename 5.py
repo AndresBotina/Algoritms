a6b9ed6d4786e1f3f6d66ae7dc4602306c4faffb
Andres Botina <cacbotina@gmail.com>
2026-08-16 18:59:58 -0500

    5

---
+++ 5.py
@@ -0,0 +1,5 @@
+print("Concatenar dos cadenas")
+cadena1 = input("Ingrese la primera cadena: ")
+cadena2 = input("Ingrese la segunda cadena: ")
+resultado = cadena1 + " " + cadena2
+print("La cadena concatenada es:", resultado)
