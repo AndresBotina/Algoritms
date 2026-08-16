605ac7db427deb9abf317abf650b363af6db6141
Andres Botina <cacbotina@gmail.com>
2026-08-15 19:04:13 -0500

    4

---
+++ 4.py
@@ -0,0 +1,9 @@
+print("Condicionales")
+num1 = float(input("Ingrese un número: "))
+if num1 > 0:
+    print("El número es positivo")
+elif num1 < 0:
+    print("El número es negativo")
+else:
+    print("El número es cero")
+    
