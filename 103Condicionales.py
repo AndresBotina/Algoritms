d227e14c8845a58914d04aba9af899edce4dcba2
Andres Botina <cacbotina@gmail.com>
2026-08-18 15:12:37 -0500

    103 Condicionales

---
+++ 103Condicionales.py
@@ -0,0 +1,8 @@
+numero = int(input("Ingrese un numero para determinar si es positivo o negativo: "))
+
+if numero < 0:
+    print("El numero es negativo")
+elif numero > 0:
+    print("El numero es positivo")
+else:
+    print("El numero es cero")
