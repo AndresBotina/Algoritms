342e12af96b5b785276232627d1b1f7da8780c93
Andres Botina <cacbotina@gmail.com>
2026-08-25 15:36:08 -0500

    109 Condicionales

---
+++ 109_Condicionales.py
@@ -0,0 +1,3 @@
+genero = str(input("Ingrese su genero M para Masculino y F para Femenino: "))
+edad = int (input("Ingrese su edad: "))
+tiempo_transcurrido = int(input("Ingrese tiempo transcurrido: "))
