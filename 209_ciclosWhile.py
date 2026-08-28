4db00425d78b8c1d9d4555ae4934d8992c9daf1f
Andres Botina <cacbotina@gmail.com>
2026-08-27 21:29:41 -0500

    209 ciclos While

---
+++ 209_ciclosWhile.py
@@ -0,0 +1,10 @@
+# 1. Inicializamos la variable de control
+contador = 5
+
+# 2. El ciclo se ejecuta MIENTRAS la condición sea verdadera
+while contador > 0:
+    print(contador)
+    # 3. IMPORTANTE: Restamos 1 para acercarnos al final y evitar un ciclo infinito
+    contador -= 1
+
+print("¡Despegue!")
