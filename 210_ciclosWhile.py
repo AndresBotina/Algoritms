4ab02b89653cc6c7af85c91d206e8d281b26f2ab
Andres Botina <cacbotina@gmail.com>
2026-09-01 15:45:19 -0500

    210 ciclos While (Sin terminar)

--- 210_ciclosWhile.py
+++ 210_ciclosWhile.py
@@ -1,11 +1,6 @@
-dia_trabajado = 0
-cantidad_horas_trabajadas= 0
-dia = 0
 hora = 8000
 while True:
     cantidad_horas_trabajadas = int(input("Ingresa la cantidad de horas del empleado: "))
     if cantidad_horas_trabajadas <0:
         break
-    if cantidad_horas_trabajadas >=8:
-        dia+=1
 
