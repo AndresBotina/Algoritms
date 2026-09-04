64f56acfc6d84f3701840c73af30dd51e1ab417a
Andres Botina <cacbotina@gmail.com>
2026-09-03 20:51:41 -0500

    310 break Continue Pass

--- 310_breakContinuePass.py
+++ 310_breakContinuePass.py
@@ -1,23 +1,19 @@
-#Se requiere una aplcacion para validar la contraseña y permitir el acceso al sistema
+# Se requiere una aplicacion para validar la contraseña y permitir el acceso al sistema
 # El usuario tiene 3 intentos, sino se bloquea el acceso
 # Si la pass es incorrecta se le pide que la ingrese nuevamente
-#Si es correcta se otorga el acceso y se finaliza el programa
+# Si es correcta se otorga el acceso y se finaliza el programa
 
 password = "123abc"
-c = 0
-clave = str(input("Ingresa la contraseña, solo tienes 3 intentos: "))
-if clave == password:
-    print("!Success!")
-else:
-    while clave != password:
-        c+=1
-        if password != clave and c <=2:
-            print (f"Te quedan {3-c} intentos")
-            clave = str(input("Contraseña incorrecta, ingresala nuevamente: "))
-        elif password != clave and c >=3:
-            print("Acceso Bloqueado")
-            break
-        elif password == clave:
-            print("!Success!")
-        else:
-            pass
+max_intentos = 3
+
+for intento in range(1, max_intentos + 1):
+    clave = input(f"Ingresa la contraseña, intento {intento}/{max_intentos}: ")
+
+    if clave == password:
+        print("!Success!")
+        break
+
+    if intento < max_intentos:
+        print(f"Te quedan {max_intentos - intento} intentos")
+    else:
+        print("Acceso Bloqueado")
