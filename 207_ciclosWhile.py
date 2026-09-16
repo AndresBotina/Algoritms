ac7b18be3f8048ef3fdfcff03c8293ec73d1bd7e
Andres Botina <cacbotina@gmail.com>
2026-09-15 23:38:12 -0500

    207 ciclos While

--- 207_ciclosWhile.py
+++ 207_ciclosWhile.py
@@ -10,12 +10,14 @@ El programa debe solicitar por teclado la cantidad producida en cada dia y al fi
 promedio = 0
 c = 0
 produccion_total = 0
-for i in range(7):
-    cantidad_producida = int(input(f"Ingrese la cantidad del dia {i+1}: "))
+dia = 1
+while dia <= 7:
+    cantidad_producida = int(input(f"Ingrese la cantidad del dia {dia}: "))
     produccion_total += cantidad_producida
-    if cantidad_producida >=101:
-        c+=1
-promedio = produccion_total/7
+    if cantidad_producida > 100:
+        c += 1
+    dia += 1
+promedio = produccion_total / 7
 print(f"El promedio vendido por dia es: {promedio}")
-print("La producción total de la semana es: ",produccion_total)
+print("La producción total de la semana es: ", produccion_total)
 print(f"El numero de dias que tuvieron una produccion mayor a 100 fue: {c}")
